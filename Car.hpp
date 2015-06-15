1b8e708cf723df7855e2243345021e46117cbf84
Eduardo Henrique Arnold <eduardoarnoldh@gmail.com>
2015-06-15 19:36:21 -0300

    Changed DEBUG mode to display detected and fg cars with diff colors. Changed time parameter on car class. Testing duplicate frame removal.

--- Car.hpp
+++ Car.hpp
@@ -14,17 +14,17 @@ class Car
                 Rect r;
                 Mat im;
                 double v;
-                double lu;                 //Last update time
+                double ts;                 //TimeStamp: Last update time
                 
         public:
-                Car(Rect re, Mat i, double t) : r(re), im(i(re)), v(0), lu(t) {};
+                Car(Rect re, Mat i, double t) : r(re), im(i(re)), v(0), ts(t) {};
                 Car& operator=(const Car& c);
                 
                 int area();
                 Point2f pos();
                 double velocity();
                 
-                void plot(Mat& f);
+                void plot(Mat& f, int);
 
                 bool onScene(Mat& f);
                 bool match(Car& c);     //Assume function is being called from the 'newest' car.
